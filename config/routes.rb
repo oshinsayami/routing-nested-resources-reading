f77ddfaa4887f320659349bde7d9c9db71fe4c7d
Simran <mdrsimran@gmail.com>
2021-01-13 14:41:56 -0600

    commit tree 3963a7cfecb0624e0bbdd778db54f045ec9f42a1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :authors, only: [:show]
-
+  get 'authors/:id/posts', to: 'authors#posts_index'
+  get 'authors/:id/posts/:post_id', to: 'authors#post' 
+  resources :authors, only: [:show] do 
+    resources :posts, only: [:index, :show]
+  end
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
   root 'posts#index'
